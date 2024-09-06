049a636eb68dc86912891aa74389600125402152
unknown <rastaaci82@gmail.com>
2024-09-06 18:21:21 +0700

    membuat biodata

--- Daffa.java
+++
@@ -1,5 +0,0 @@
-public class Daffa {
-    public static void main(String[] args) {
-        System.out.println("Justin unch");
-    }
-}
---
+++ Rasta.java
@@ -0,0 +1,14 @@
+public class Rasta {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama      : Rasta Nura Rayani Saputri");
+        System.out.println("NIM       : 244107060106");
+        System.out.println("Kelas     : SIB 1C");
+        System.out.println("No Absen  : 19");
+        System.out.println("Prodi     : D-IV Sistem Informasi Bisnis");
+        System.out.println("No HP     : 085707621793");
+        System.out.println("Hobi      : Suka Pergi ke kafe");
+    }
+}
