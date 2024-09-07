65161cb59fb8b2706ac49e96b3d66fcff4dfbc51
RafiZFSIB1C <68289590+R291-PIC@users.noreply.github.com>
2024-09-07 15:14:36 +0700

    Add files via upload

---
+++ Biodata Rafi.java
@@ -0,0 +1,14 @@
+public class Rafi {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama      : Rafi Zeta Fauzan");
+        System.out.println("NIM       : 244107060120");
+        System.out.println("Kelas     : SIB 1C");
+        System.out.println("No Absen  : 18");
+        System.out.println("Prodi     : D-IV Sistem Informasi Bisnis");
+        System.out.println("No HP     : 081217035714");
+        System.out.println("Hobi      : Bermain Games dan Membaca Buku");
+    }
+}
