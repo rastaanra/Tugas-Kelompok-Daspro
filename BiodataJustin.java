81926d610f56f8e1eecde29480b399d6206b0ba1
JustinLaiya <laiyajustin@gmail.com>
2024-09-07 09:09:27 +0700

    ti fas

---
+++ BiodataJustin.java
@@ -0,0 +1,15 @@
+public class BiodataJustin {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama            :Justin Fadly Laiya");
+        System.out.println("NIM             :244107060142");
+        System.out.println("Kelas           :1C");
+        System.out.println("No Absen        :10");
+        System.out.println("Prodi           :D4 Sistem Informasi Bisnis");
+        System.out.println("Alamat          :Jl.KH,Malik Dalam");
+        System.out.println("No HP           :082136521560");
+        System.out.println("Hobi            :Olahraga");
+    }
+}
